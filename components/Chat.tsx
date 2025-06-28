5c3d636d4ee0375a186d38ab38e21007037a637d
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2025-06-28 11:02:24 +0530

    feat: Add open ai

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,23 +1,15 @@
 "use client";
 import React from "react";
 import { Textarea } from "./ui/textarea";
-import { generateText } from "ai"
-import { openai } from "@ai-sdk/openai"
+import {  UIMessage } from "ai";
 
+
+import { useChat } from "@ai-sdk/react";
 
 export default function Chat() {
-
+  const { messages, input, handleInputChange, handleSubmit }: any = useChat({});
 
   
-  const handleSend = async () => {
-    const { text } = await generateText({
-      model: openai("o3-mini"),
-      prompt: "What is love?"
-    })
-    console.log(text)
-  }
-
-
 
   return (
     <div className="flex flex-col h-full  items-center justify-between p-6">
@@ -25,32 +17,34 @@ export default function Chat() {
         <span className="font-semibold">chat</span>
       </div>
 
-       {/* Chat Messages */}
-       <div className="flex flex-col h-full justify-end gap-2 overflow-y-auto w-full">
+      {/* Chat Messages */}
+      <div className="flex flex-col h-full justify-end gap-2 overflow-y-auto w-full">
         <span className="flex flex-col items-start gap-2  border-gray-200 p-4 w-full">
           Who is the current president of the United States?
-          </span>
+        </span>
 
-          <span className="flex flex-col items-start gap-2  border-gray-200 p-4 w-full">
+        <span className="flex flex-col items-start gap-2  border-gray-200 p-4 w-full">
           Who is the current president of the United States?
-          </span>
+        </span>
+        {messages.map((message: UIMessage) => (
+          <div key={message.id}>
+            {message.role === "user" ? "User: " : "AI: "}
+            {message.parts.map((part, index) =>
+              part.type === "text" ? <span key={index}>{part.text}</span> : null
+            )}
+          </div>
+        ))}
       </div>
 
-
-     
       <div className="grid w-full gap-3">
         <Textarea placeholder="Type your message here." id="message-2" />
         <p className="text-muted-foreground text-sm">
           Your message will be copied to the support team.
         </p>
-        <button
-          onClick={() => {
-            handleSend()
-          }}
-          className="h-10 rounded-lg border border-gray-200 p-2 hover:bg-gray-200"
-        >
-          Send
-        </button>
+        <form onSubmit={handleSubmit}>
+          <input name="prompt" value={input} onChange={handleInputChange} />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </div>
   );
